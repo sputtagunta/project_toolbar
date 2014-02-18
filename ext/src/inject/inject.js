72c35cf511627892fe3cc80f621b05edaa3e529d
Sravan Puttagunta <sravan@solfice.com>
2014-02-18 14:05:23 -0800

    able to grab html content for inbox table body

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -9,7 +9,27 @@ chrome.extension.sendMessage({}, function(response) {
 		// ----------------------------------------------------------
 
         
+        //*
+        var emails = document.getElementsByClassName('InboxTableBody');
+        parse_inbox_table_body(emails);
+
 
 	}
 	}, 10);
 });
+
+function parse_inbox_table_body(emails) {
+
+    for (var i = 0; i < emails.length; i++) {
+        emails[i].addEventListener('click', (function(i) {
+            return function() {
+                this.innerHTML = 'New Text';
+            };
+        })(i), false);
+
+        console.log(emails[i].innerHTML);
+        
+    }
+    //*/
+
+}
