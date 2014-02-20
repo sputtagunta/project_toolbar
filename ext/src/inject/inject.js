5a9ce7520882cfbc3d5adbeba12df50443d1ce09
Sravan Puttagunta <sravan.puttagunta@gmail.com>
2014-02-19 23:10:57 -0800

    added files for pixel injection.
    added sha1 for id generation
    
    TODO : bugs, ctrl + enter doesn't capture sent email

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -1,24 +1,114 @@
 chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
+    	if (document.readyState === "complete") {
+    		clearInterval(readyStateCheckInterval);
 
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		// console.log("Hello. This message was sent from scripts/inject.js");
-		// ----------------------------------------------------------
-
-
-        $(document).ready( function () {
-            setTimeout( function() {
-                parse_inbox_table_body();
-            }, 2000);
-        });
-
-	}
+            // starts the project_toolbar code
+            inject_event_handlers();
+            inject_dom_elements();
+            parse_inbox_table_body();
+    	}
 	}, 10);
 });
 
+function inject_dom_elements() {
+    
+}
+
+
+function inject_event_handlers() {
+    // if the user clicks the send button
+    $('#SendMessage').click( function() {
+        metadata = update_metadata();
+        generate_pixels(metadata);
+    });
+
+    // if the user uses ctrl + enter on the main page
+    $(document).keydown(function(event) {
+        key_event(event);
+    });
+
+    // if the user uses ctrl + enter on the iframe
+    
+    // create interval for checking if iframe element loaded
+    var iframe_interval = setInterval(function() {
+        // if the iframe is loaded enter the if statement
+        iframeDoc = $('#ComposeRteEditor_surface').contents().get(0); 
+
+        if(iframeDoc != undefined && iframeDoc != null) {
+            // clear the interval handler if the iframe is loaded
+            clearInterval(iframe_interval);
+            iframe_interval = 0;
+
+            var iframeDoc = $('#ComposeRteEditor_surface').contents().get(0);
+            $("body", iframeDoc).keydown(function(event) {
+                // forward event to key_event function
+                key_event(event);
+            });
+        }
+    }, 100); // check every 100 ms
+}
+
+function key_event(e) {
+    console.log("keycode: " + e.keyCode);
+    if (e.ctrlKey && e.keyCode == 13) {
+        metadata = update_metadata();
+        generate_pixels(metadata);
+    }
+}
+
+// updates metadata every 100 ms
+function update_metadata() {
+
+    metadata = {};
+    metadata["from"] = $('.FromContainer').find(".Address").html()
+    metadata["to"] = get_emails("#toCP");
+    metadata["cc"] = get_emails("#ccCP");
+    metadata["action"] = "sent";
+    metadata["bcc"] = get_emails("#bccCP");
+    metadata["subject"] = $("#fSubject").val();
+    metadata["device_timestamp"] = Math.floor((new Date()).getTime() / 1000);
+    email_body = $("#ComposeRteEditor_surface").contents().find('body').html();
+    metadata["id"] = compute_hash(metadata, email_body);
+    //metadata["message_body"] = $("#ComposeRteEditor_surface").contents().find('body').html();
+    return metadata;
+}
+
+function compute_hash(metadata, email_body) {
+    var str = "";
+    for (var k in metadata) {
+        if (typeof metadata[k] == "number") {
+            str += metadata[k].toString();
+        } else {
+            str += metadata[k];
+        }
+    }
+    str += email_body;
+
+    return SHA1(str);
+
+}
+
+
+
+function generate_pixels(metadata) {
+    var b = $.param(metadata);
+    msg_body = $("#ComposeRteEditor_surface").contents().find('body').html();
+    if(msg_body != null && msg_body != undefined) {
+        // if the custom blocker exists, truncate the text at the blocker
+        // append the data
+        image = "<img src='https://sravan.us/images/img.php?"+b+"' width='1' height='1'>";
+        $("#ComposeRteEditor_surface").contents().find('body').html(msg_body + image);
+    }
+}
+
+function get_emails(element) {
+    var to = [];
+    $(element).find(".cp_ctBtn").each(function() {
+        to.push($(this).attr("title"));
+    });
+    return to;
+}
 
 function parse_inbox_table_body() {
     $(".InboxTableBody" ).find( "li" ).click( function() {
@@ -53,6 +143,7 @@ function parse_email_by_id(id) {
         metadata["importance"] = get_email_data(data, "importance");
         metadata["email_body"] = data;
         metadata["open_timestamp"] = Math.round(new Date().getTime()/1000.0);
+        metadata["recipient"] = get_email_data(data, "x-sid-pra");
 
         img_url = "https://sravan.us/images/img.php?"+$.param(metadata);
         $('#mpf0_MsgContainer').html($('#mpf0_MsgContainer').html()+"<img src='"+img_url+"'>");
